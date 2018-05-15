b13b9e522ebb9cb0f52e2284c6327a216cdbcdde
Ngoc Chien Nguyen <chien.nguyenngoc89@gmail.com>
2018-05-15 10:45:07 +0700

    complete defer example

--- Golang/Basic/defer.go
+++ Golang/Basic/defer.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func do() {
+	n := 1
+	defer fmt.Println("defer 1", n)
+
+	n++
+	defer fmt.Println("defer 2", n)
+
+	n++
+	defer fmt.Println("defer 3", n)
+}
+
+func checkErr() {
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Println("An error occured: ", err)
+		}
+	}()
+
+	err = errors.New("Bad thing happened")
+}
+
+func main() {
+	do()
+	checkErr()
+}
